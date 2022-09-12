6a42c2f51fca5d3b2f33f450c25d045cdde3a9c0
Soomin Lee <dinggididududu@gmail.com>
2022-09-12 01:18:52 -0400

    add buttons class

---
+++ A11/src/NumPuz/Buttons.java
@@ -0,0 +1,23 @@
+/**
+ * File Name : Main.java
+ * Identification : Soomin113
+ * Course : CST8221 - JAP, Lab Section : 302
+ * Assignment : A11
+ * Professor : Paulo Sousa
+ * Date : 
+ * Compiler : Eclipse IDE
+ * Purpose : Number Puzzle - Buttons class
+ */
+package NumPuz;
+/**
+ * Class Name : Buttons
+ * Methods List :
+ * Constants List : 
+ * Purpose :
+ * 
+ *  @author Soomin
+ *
+ */
+public class Buttons {
+	
+} //Buttons class end
